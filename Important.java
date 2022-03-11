1300c285271abc36300095ffae0ef605cf8319df
Bharath Ravelli <88048287+bharathravelli-419@users.noreply.github.com>
2022-03-11 23:39:06 +0530

    ConcatinationVsAdding

---
+++ Important.java
@@ -0,0 +1,11 @@
+public class Important
+{
+  public static void main(String[] args)
+ {
+   int x =10,y =20;
+  System.out.println("Without sum is :"+x+y);
+  System.out.println("The Actual sum is :"+(x+y));
+
+}
+
+}
