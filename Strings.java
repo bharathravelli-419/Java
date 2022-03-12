df8e3db90448b4cb5291572cc38a027efebe4f7b
Bharath Ravelli <88048287+bharathravelli-419@users.noreply.github.com>
2022-03-12 16:29:25 +0530

    Basic Stuff

---
+++ Strings.java
@@ -0,0 +1,16 @@
+public class Strings
+{
+  public static void main(String[] args)
+{
+
+
+  char c[] = {'H','e','l','l','o'};
+  String str = new String(c,1,4);
+ System.out.println(str);
+   
+
+  }
+
+}
+
+// op:ello
