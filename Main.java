e62cf8a56ced81b237eea2a8c5d5e48beafc4ec6
Bharath Ravelli <88048287+bharathravelli-419@users.noreply.github.com>
2022-03-12 16:39:53 +0530

    String Methods

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+  public static void main(String[] args)
+{
+
+  String Methods in JAVA
+Strings in JAVA are immutable
+
+    str.length()
+    str.toLowerCase()
+    str.toUpperCase()
+    str.trim().........removes Blank spaces
+    str.substring(int begin)
+    str.substring(int begin_index,int end_index)
+    str.replace(char old_char,char new_char)
+    str.startsWith(String s)
+    str.charAt(int index)
+    str.indexOf(String s)
+    str.lastIndexOf(String s)
+    str.equals(String s)
+    str.valueOf(int i)
+ 
+
+  }
+
+}
+
+// op:ello
